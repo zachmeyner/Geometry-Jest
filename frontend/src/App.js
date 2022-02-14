4d00d6ac7476d3643226360390feda83a0a6999b
zachmeyner <zacharymeyner@gmail.com>
2022-02-14 00:32:05 -0500

    Setup Rust React and got the the bare minimum rocket cors working

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const getData = async () => {
+    const response = await axios("http://localhost:8000/myrocket");
+  console.log(response.data);
+};
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Geometry Jest with Rust and React
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
