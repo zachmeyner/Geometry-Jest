b9375a8a4f92bbfebf511b8d4f941a8fc19500c1
zachmeyner <zacharymeyner@gmail.com>
2022-03-27 20:14:07 -0400

    Made the compiler shut the hell up about unused things

--- server/src/api/v1/handlers/login.rs
+++ server/src/api/v1/handlers/login.rs
@@ -1,4 +1,4 @@
-use rocket::serde::json::{json, Json, Value};
+/*use rocket::serde::json::{json, Json, Value};
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
@@ -6,6 +6,7 @@ struct LoginForm<'r> {
     username: &'r str,
     password: &'r str,
 }
+*/
 
 // TODO: Create a POST request that recieves username and password and attempt to login using those credientials
 // TODO This will acquire a lot more integration with Diesel and MySQL than we have at the current time, which is 0.
