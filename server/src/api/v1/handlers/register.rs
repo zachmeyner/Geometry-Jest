bef82e1a313ff4d65e0281fffd157a8e538fc007
zachmeyner <zacharymeyner@gmail.com>
2022-03-27 20:14:25 -0400

    Removed asciiStr as a dependency

--- server/src/api/v1/handlers/register.rs
+++ server/src/api/v1/handlers/register.rs
@@ -1,7 +1,7 @@
-use ascii::{AsciiStr, AsciiString};
-use hex;
-use rand::{thread_rng, Rng};
-use rocket::http::{ContentType, Header, Status};
+use rand::distributions::{Alphanumeric, Distribution};
+// use hex;
+use rand::thread_rng;
+use rocket::http::Status;
 use rocket::response::{content, status};
 use rocket::serde::json::Json;
 use serde::Deserialize;
@@ -9,8 +9,8 @@ use sha2::{Digest, Sha256};
 
 #[derive(Debug, Deserialize)]
 pub struct RegInfo<'r> {
-    username: &'r [u8],
-    password: &'r [u8],
+    username: &'r str,
+    password: &'r str,
 }
 
 // Takes in a username and password, salts then hashes the password.
@@ -20,24 +20,27 @@ pub struct RegInfo<'r> {
 #[post("/register", format = "json", data = "<reg>")]
 pub async fn register(reg: Json<RegInfo<'_>>) -> status::Custom<content::Json<&'static str>> {
     let mut rng = thread_rng();
-    let ascii_pass = AsciiString::from_ascii(reg.password).unwrap();
 
     // FIll Array of unsigned 8 bit ints with valid ascii characters (32 -> 127)
-    let mut salt: [u8; 32] = [0; 32];
-    for i in 0..32 {
-        salt[i] = rng.gen_range(32..126);
-    }
+    let salt: String = Alphanumeric
+        .sample_iter(&mut rng)
+        .take(32)
+        .map(char::from)
+        .collect();
 
     // Salts the password with the characters from the 64 byte array
-    let salt_string_slice = AsciiStr::from_ascii(&salt).unwrap();
-    let salted_string = ascii_pass + salt_string_slice;
+    let salted_string = format!("{}{}", reg.password, salt);
 
     // Hashes the Salted Password
     let mut hasher = Sha256::new();
-    hasher.update(AsciiStr::as_bytes(&salted_string));
-    let hash = hasher.finalize();
+    hasher.update(salted_string.as_bytes());
+    let _hash = hasher.finalize();
 
-    // println!("{:#?} \n {:#?}", salt_string_slice, hex::encode(hash));
+    println!(
+        "{:#?} \n {:#?}",
+        salted_string.as_bytes(),
+        hex::encode(_hash)
+    );
 
     // * Temporary output until everything starts coming together
 
