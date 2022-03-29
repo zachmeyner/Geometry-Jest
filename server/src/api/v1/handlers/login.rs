d065a8de6e11fc9807e4928f6dafd246d1e4b014
zachmeyner <zacharymeyner@gmail.com>
2022-03-28 21:18:05 -0400

    Started work on login (NOT FINISHED)

--- server/src/api/v1/handlers/login.rs
+++ server/src/api/v1/handlers/login.rs
@@ -1,12 +1,35 @@
-/*use rocket::serde::json::{json, Json, Value};
-use serde::Deserialize;
+use rocket::http::Status;
+use rocket::response::{content, status};
+use rocket::serde::json::Json;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize)]
-struct LoginForm<'r> {
+#[derive(Debug, Deserialize, Serialize)]
+pub struct LoginForm<'r> {
     username: &'r str,
-    password: &'r str,
+    pw: &'r str,
+    iat: u32,
 }
-*/
+
+#[derive(Debug, Serialize)]
+pub struct Claims<'r> {
+    username: &'r str,
+    iat: u32,
+}
 
 // TODO: Create a POST request that recieves username and password and attempt to login using those credientials
 // TODO This will acquire a lot more integration with Diesel and MySQL than we have at the current time, which is 0.
+#[post("/login", format = "json", data = "<args>")]
+pub async fn login(args: Json<LoginForm<'_>>) -> status::Custom<content::Json<String>> {
+    let go: bool = crate::controllers::users::check_if_exists(args.username).await;
+
+    if go {
+        let token_str =
+            crate::tools::gentoken::gen_auth_key(args.username.to_string(), args.iat).await;
+
+        let ret = format!("{{ \"Token\": \"{}\" }}", token_str);
+
+        status::Custom(Status::Accepted, content::Json(ret))
+    } else {
+        status::Custom(Status::Unauthorized, content::Json("".to_string()))
+    }
+}
