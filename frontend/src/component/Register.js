3392fa45bb8fa19f944924f766383074839ff74c
ddr-09 <rddaniel29@hotmail.com>
2022-04-14 12:53:59 -0400

    fixed user,pw now works, working on register post

--- frontend/src/component/Register.js
+++ frontend/src/component/Register.js
@@ -2,26 +2,26 @@ import React, { useState } from "react";
 import { Container, Form, Button, Row, Col } from "react-bootstrap";
 import { CSSTransition } from 'react-transition-group';
 import axios from 'axios';
+import click from './../static/clickSubmitScore.mp3'
 export default function Register() {
     const [displayForm, setDisplayForm] = useState('d-none');
     const [displayFormBool, setDisplayFormBool] = useState(false);
-    const [dataSent, setDataSent] = useState(["", "", ""]);
-    const url = 'http://localhost:8000/'
+    const [user, setUser] = useState("");
+    const [pw, setPw] = useState("");
+    const url = 'http://127.0.0.1:8000/';
     function ShowForm() {
         if (displayForm !== 'd-none') {
             setDisplayForm('d-none');
             setDisplayFormBool(false);
         } else {
+            new Audio(click).play()
             setDisplayForm('d-flex');
             setDisplayFormBool(true);
         }
     }
-    function HandleRegister(event) {
-        var newLogin = [event.target[0].value, event.target[1].value, Date.now()];
-        setDataSent(newLogin);
-        event.preventDefault();
+    function HandleRegister() {
         axios
-            .post(url.concat(event.nativeEvent.submitter.name), dataSent, {
+            .post(url.concat('register'), user, pw, Date.now(), {
                 headers: {
                     Accept: "application/json",
                     "Content-Type": "application/json;charset=UTF-8",
@@ -30,7 +30,6 @@ export default function Register() {
             .then(({ data }) => {
                 console.log(data);
             });
-
     }
 
     return (
@@ -43,14 +42,17 @@ export default function Register() {
                 unmountOnExit
             >
                 <Container className={`gj-bg gj-login-form row ${displayForm} position-relative rounded`}>
-                    <Form method="POST" action={url} onSubmit={HandleRegister} >
+                    <Form method="POST" action={url} >
                         <Row className="p-1" >
                             <Col>
-                                <Form.Control autoFocus placeholder="Username" name="user" />
-
+                                <Form.Control autoFocus placeholder="Username" name="user" onChange={(e) => {
+                                    setUser(e.target.value);
+                                }} />
                             </Col>
                             <Col>
-                                <Form.Control placeholder="Password" name="pass" />
+                                <Form.Control placeholder="Password" name="pass" onChange={(e) => {
+                                    setPw(e.target.value);
+                                }} />
                             </Col>
                             <Col sm={1}>
                                 <button type="button" className="btn-close btn-close-white" aria-label="Close" onClick={ShowForm}></button>
@@ -58,7 +60,7 @@ export default function Register() {
                         </Row>
                         <Row className="p-1">
                             <Col className="float-start m-0">
-                                <Button variant="warning" type="submit" name="register">
+                                <Button variant="warning" name="register" onClick={HandleRegister}>
                                     Register
                                 </Button>
                             </Col>
