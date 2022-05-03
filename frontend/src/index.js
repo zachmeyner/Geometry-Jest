b5b54795649bd2e8e7b77373690ed02d79452d50
ddr-09 <rddaniel29@hotmail.com>
2022-05-03 16:32:49 -0400

    removed unused

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Container } from "react-bootstrap";
-
 //import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'; // adding boostrap css
 ReactDOM.render(
